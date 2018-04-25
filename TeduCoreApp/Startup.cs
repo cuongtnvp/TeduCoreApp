96fbb154cce35363f740931d06bd13f206585c60
cuongdevpro <cuongdevpro@gmail.com>
2018-04-25 17:37:57 +0700

    Seeding

--- TeduCoreApp/Startup.cs
+++ TeduCoreApp/Startup.cs
@@ -11,6 +11,8 @@ using Microsoft.Extensions.DependencyInjection;
 using TeduCoreApp.Data;
 using TeduCoreApp.Models;
 using TeduCoreApp.Services;
+using TeduCoreApp.Data.EF;
+using TeduCoreApp.Data.Entities;
 
 namespace TeduCoreApp
 {
@@ -26,11 +28,12 @@ namespace TeduCoreApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"),
+                o=>o.MigrationsAssembly("TeduCoreApp.Data.EF")));
 
-            services.AddIdentity<ApplicationUser, IdentityRole>()
-                .AddEntityFrameworkStores<ApplicationDbContext>()
+            services.AddIdentity<AppUser, AppRole>()
+                .AddEntityFrameworkStores<AppDbContext>()
                 .AddDefaultTokenProviders();
 
             // Add application services.
